5d469d7307d6d2cf00b8a809e842a35b9bad32c6
Nikos Verschore <nikos@uwsoftware.be>
2026-03-02 10:53:04 +0100

    no unlooping + skip namespace resolution + read less for path order

--- app/Parser.php
+++ app/Parser.php
@@ -15,11 +15,11 @@ final class Parser
         $left = "";
         $read = 0;
 
-        $output = str_repeat(chr(0), $pathCount*$dateCount);
+        $output = \str_repeat(\chr(0), $pathCount*$dateCount);
 
         $next = [];
         for($i=0; $i!=255;$i++) {
-            $next[chr($i)] = chr($i+1);
+            $next[\chr($i)] = \chr($i+1);
         }
 
         $file = new SplFileObject($inputPath);
@@ -32,20 +32,20 @@ final class Parser
             $lenAsked = $read + Parser::$READ_CHUNK >= $length ? $length - $read : Parser::$READ_CHUNK;
             $buffer = $file->fread($lenAsked);
 
-            if(substr($buffer, -1) != "\n") {
+            if(\substr($buffer, -1) != "\n") {
                 $extra = $file->fgets()."\n";
-                $lenAsked += strlen($extra);
+                $lenAsked += \strlen($extra);
                 $buffer .= $extra;
             }
 
             $nextPos = -1;
-            if($start == 0 && $chunks++ < 2) {
+            if($start == 0 && $chunks++ < 1) {
                 while($nextPos+10 < $lenAsked) {
                     $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
+                    $nextPos = \strpos($buffer, "\n", $nextPos + 52);
                     
-                    $path = substr($buffer, $pos + 30, $nextPos - $pos - 56);
-                    $date = substr($buffer, $nextPos - 22, 7);
+                    $path = \substr($buffer, $pos + 30, $nextPos - $pos - 56);
+                    $date = \substr($buffer, $nextPos - 22, 7);
 
                     $pathId = $paths[$path];
                     
@@ -56,52 +56,10 @@ final class Parser
                 }
             }
             else {
-                while($nextPos+10 < $lenAsked-470_000) {
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-
-                    $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
-                    $output[$index] = $next[$output[$index]];
-                }
-
                 while($nextPos+10 < $lenAsked) {
                     $pos = $nextPos;
-                    $nextPos = strpos($buffer, "\n", $nextPos + 52);
-                    $index = $dates[substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[substr($buffer, $pos + 30, $nextPos - $pos - 56)];
+                    $nextPos = \strpos($buffer, "\n", $nextPos + 52);
+                    $index = $dates[\substr($buffer, $nextPos - 22, 7)]*$pathCount+$paths[\substr($buffer, $pos + 30, $nextPos - $pos - 56)];
                     $output[$index] = $next[$output[$index]];
                 }
             }
@@ -113,36 +71,36 @@ final class Parser
     }
 
     public function convert($input, $dates, $order) {
-        return $input.pack("v*", ...array_keys($order));
+        return $input.\pack("v*", ...\array_keys($order));
     }
 
     public function partParallel(string $inputPath, int $start, int $length, $dates, $paths, $pathCount, $dateCount) {
-        list($readChannel, $writeChannel) = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
-        $pid = pcntl_fork();
+        list($readChannel, $writeChannel) = \stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+        $pid = \pcntl_fork();
 
         if ($pid == 0) {
-            fclose($readChannel);
+            \fclose($readChannel);
             $output = $this->partParse($inputPath, $start, $length, $dates, $paths, $pathCount, $dateCount);
-            fwrite($writeChannel, $output);
+            \fwrite($writeChannel, $output);
             exit();
         }
 
-        fclose($writeChannel);
+        \fclose($writeChannel);
         return $readChannel;
     }
 
     public function partReadParallel($thread, $outputLength) {
         $output = "";
-        while(!feof($thread)) {
-            $output .= fread($thread, Parser::$READ_CHUNK);
+        while(!\feof($thread)) {
+            $output .= \fread($thread, Parser::$READ_CHUNK);
         }
 
-        return [substr($output, 0, $outputLength), unpack("v*", substr($output, $outputLength))];
+        return [\substr($output, 0, $outputLength), \unpack("v*", \substr($output, $outputLength))];
     }
 
     public function parse(string $inputPath, string $outputPath): void
     {
-        gc_disable();
+        \gc_disable();
 
         // Prepare arrays
         $m2d = [0, 32, 30, 32, 31, 32, 31, 32, 32, 31, 32, 31, 32];
@@ -170,7 +128,7 @@ final class Parser
         $paths = [];
         $pathCount = 0;
         foreach(Visit::all() as $page) {
-            $uri = substr($page->uri, 29);
+            $uri = \substr($page->uri, 29);
             $paths[$uri] = $pathCount++;
         }
 
@@ -179,7 +137,7 @@ final class Parser
         $start = 0;
         $file = new SplFileObject($inputPath);
         $file->setFlags(SplFileObject::READ_AHEAD | SplFileObject::DROP_NEW_LINE | SplFileObject::SKIP_EMPTY);
-        $length = ceil(filesize($inputPath)/Parser::$CORES);
+        $length = \ceil(\filesize($inputPath)/Parser::$CORES);
         for($i=0; $i!=Parser::$CORES; $i++) {
             $file->fseek($length*$i+$length);
             $file->fgets();
@@ -202,19 +160,19 @@ final class Parser
 
         $pathsJson = [];
         foreach(Visit::all() as $page) {
-            $uri = substr($page->uri, 25);
-            $short = substr($page->uri, 29);
+            $uri = \substr($page->uri, 25);
+            $short = \substr($page->uri, 29);
             $pathsJson[$paths[$short]] = "\n    \"\\/blog\\/".$uri.'": {';
         }
 
-        $output = array_fill(0, $pathCount*$dateCount, 0);
+        $output = \array_fill(0, $pathCount*$dateCount, 0);
 
         // Read threads
         $first = $threads[0];
         $read = []; $write = []; $except = [];
-        while(count($threads) != 0) {
+        while(\count($threads) != 0) {
             $read = $threads;
-            stream_select($read, $write, $except, 5);
+            \stream_select($read, $write, $except, 5);
             foreach($read as $i => $thread) {
                 if($thread == $first) {
                     list($data, $sortedPaths) = $this->partReadParallel($thread, $pathCount*$dateCount);
@@ -224,7 +182,7 @@ final class Parser
                 }
 
                 for($j=0; $j!=$pathCount*$dateCount; $j+=1) {
-                    $output[$j] += ord($data[$j]);
+                    $output[$j] += \ord($data[$j]);
                 }
                 unset($threads[$i]);
             }
@@ -251,6 +209,6 @@ final class Parser
         }
         $buffer .= "\n}";
 
-        file_put_contents($outputPath, $buffer);
+        \file_put_contents($outputPath, $buffer);
     }
 }
